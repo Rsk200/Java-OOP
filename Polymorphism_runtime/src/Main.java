cff28d90948cafce859801741f521444b68d382e
Rsk200 <115521165+Rsk200@users.noreply.github.com>
2024-07-26 15:22:24 +0600

    Add files via upload

---
+++ Polymorphism_runtime/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        System.out.println(calc.add(10, 20));
+        System.out.println(calc.add(10,20,30));
+        System.out.println(calc.add(2.5,4.2,32.2));
+
+        }
+    }
